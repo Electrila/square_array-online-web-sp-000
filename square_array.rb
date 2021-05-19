7d340c141dc70a2a4f11e19190f14b433e5144c3
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-19 12:13:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,8 @@ def square_array(array)
   array.each { |num| squared_nums << num ** 2 }
   squared_nums
 end
+
+# Squared using #collect
+def square_array(array)
+  array.collect { |num| num ** 2 }
+end
